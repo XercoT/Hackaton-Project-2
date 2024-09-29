c1ea7760d56ba4ded9493469384409efc953cb37
XercoT <raymanti9@gmai.com>
2024-09-29 09:20:42 -0400

    done

---
+++ HackatonProject/Misc/FirebaseManager.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseManager.swift
+//  HackatonProject
+//
+//  Created by Xerco Tchouankeu on 9/29/24.
+//
+
+import SwiftUI
+
+struct FirebaseManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirebaseManager()
+}
